e53be10b9708ee6dbf88357c1e4ed67e3c7e06a5
Kai993 <huf0618@icloud.com>
2018-08-11 09:34:50 +0900

    add test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_将来の質問の場合はFalseを返す(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_pub_dateが1日前の質問についてはFalseを返す(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_pub_dateが最終日以内にある質問に対してTrueを返す(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
